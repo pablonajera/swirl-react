e9765a310df2154b1c680a0a5105909ef545a00d
Pablo Najera <pablo@najera.cc>
2023-08-08 22:20:47 -0400

    fixing pick to not add empty keys

--- src/utils/pick.ts
+++ src/utils/pick.ts
@@ -1,7 +1,9 @@
 export function pick<T, K extends keyof T> (obj: T, keys: K[]): Pick<T, K> {
   const newObj: any = {}
   keys.forEach((key) => {
-    newObj[key] = obj[key]
+    if (obj[key] != null) {
+      newObj[key] = obj[key]
+    }
   })
   return newObj
 }
